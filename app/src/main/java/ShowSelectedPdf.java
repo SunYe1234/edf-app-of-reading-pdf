d86f6c75f5bca7bb38a57e703c3cceea3f4ad72a
yezi <sun.ye@hotmail.com>
2019-07-02 16:25:09 +0200

    the first version:open a pdf stored under Download

---
+++ app/src/main/java/ShowSelectedPdf.java
@@ -0,0 +1,17 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.xiaolong.pdfiumpdfviewer.R;
+
+/**
+ * Created by yezi on 2019/7/2.
+ */
+
+public class ShowSelectedPdf extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_pdf);
+    }
+
+}
